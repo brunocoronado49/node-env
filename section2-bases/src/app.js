cfb12e3ca57ec361575a91e5623eccc8d02b6826
brunocoronado49 <francorangelcoronado@gmail.com>
2025-12-14 22:22:36 -0600

    Bases de nodejs: factory functions y patron adapter

--- section2-bases/src/app.js
+++ section2-bases/src/app.js
@@ -1,13 +1,8 @@
-// const { emailTemplate } = require('./js-foundation/01-template.js');
-// require('./js-foundation/02-destructuring.js')
-const { getUserById } = require('./js-foundation/04-arrow-functions.js')
+const { getAge, getUUID } = require('./plugins');
 
-// console.log(emailTemplate);
+const { buildMakePerson } = require('./js-foundation/05-factory-functions');
 
-const id = 1;
-
-getUserById(id, (error, user) => {
-  if (error) throw new Error(error);
-  console.log(user);
-});
-
+const obj = {name: 'John Wick', birthdate: '1997-10-06'};
+const makePerson = buildMakePerson(getUUID, getAge);
+const johnWick = makePerson(obj);
+console.log({johnWick});
