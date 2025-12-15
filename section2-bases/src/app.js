a6f241aa792ff974f3f488d46658c1325232191a
brunocoronado49 <francorangelcoronado@gmail.com>
2025-12-14 20:22:08 -0600

    Bases de node (repasando arrow functions)

--- section2-bases/src/app.js
+++ section2-bases/src/app.js
@@ -1,5 +1,13 @@
-const { emailTemplate } = require('./js-foundation/01-template.js');
+// const { emailTemplate } = require('./js-foundation/01-template.js');
+// require('./js-foundation/02-destructuring.js')
+const { getUserById } = require('./js-foundation/04-arrow-functions.js')
 
-console.log(emailTemplate);
+// console.log(emailTemplate);
 
-// TODO: Avanzar en la clase de nodemon
+const id = 1;
+
+getUserById(id, (error, user) => {
+  if (error) throw new Error(error);
+  console.log(user);
+});
+
---
+++ section2-bases/src/js-foundation/02-destructuring.js
@@ -0,0 +1,8 @@
+// console.log(process.env);
+
+const { SHELL } = process.env;
+// console.log(SHELL);
+
+const caracters = ['leon', 'claire', 'jill'];
+const [,, jill] = caracters;
+// console.log(leon);
