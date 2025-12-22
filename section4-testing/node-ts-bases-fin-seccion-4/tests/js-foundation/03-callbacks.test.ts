413c669c0fd83d3e04914343247fd947580f7124
brunocoronado49 <francorangelcoronado@gmail.com>
2025-12-22 10:27:56 -0600

    Bases de node: Testing (factory)

--- section4-testing/node-ts-bases-fin-seccion-4/tests/js-foundation/03-callbacks.test.ts
+++ section4-testing/node-ts-bases-fin-seccion-4/tests/js-foundation/03-callbacks.test.ts
@@ -10,4 +10,13 @@ describe('js-foundation/03-callbacks', () => {
       expect(user).toBeUndefined();
     });
   });
+
+  test('getUserById should return John Doe with id 1', () => {
+    const id = 1;
+
+    getUserById(id, (error, user) => {
+      expect(error).toBeUndefined();
+      expect(user?.name).toBe('John Doe');
+    });
+  });
 });
