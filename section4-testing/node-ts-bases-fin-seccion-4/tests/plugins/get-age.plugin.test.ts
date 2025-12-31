20a4f0e1636546598de3e4a5fee8b2b3639e86a9
brunocoronado49 <francorangelcoronado@gmail.com>
2025-12-31 15:03:59 -0600

    Testing: logger

--- section4-testing/node-ts-bases-fin-seccion-4/tests/plugins/get-age.plugin.test.ts
+++ section4-testing/node-ts-bases-fin-seccion-4/tests/plugins/get-age.plugin.test.ts
@@ -1,14 +1,21 @@
-import { describe, expect, test } from '@jest/globals';
+import { describe, expect, jest, test } from '@jest/globals';
 import { getAge } from '../../src/plugins/get-age.plugin';
 
 describe('plugins/get-age.plugin', () => {
   test('getAge should return the age of person', () => {
     const bday = '1997-10-06';
     const age = getAge(bday);
+    const calculatedAge = new Date().getFullYear() - new Date(bday).getFullYear();
 
-    expect(typeof age).toBe('number');
-    expect(age).toBe(28);
+    expect(age).toBe(calculatedAge);
   });
 
-  //TODO: Terminar el SpyOn
+  test('getAge should return 0 years', () => {
+    const spy = jest.spyOn(Date.prototype, 'getFullYear').mockReturnValue(1997);
+    const bday = '1997-10-06';
+    const age = getAge(bday);
+
+    expect(age).toBe(0);
+    expect(spy).toHaveBeenCalled();
+  });
 });
---
+++ section4-testing/node-ts-bases-fin-seccion-4/tests/plugins/get-id.plugin.test.ts
@@ -0,0 +1,11 @@
+import { describe, expect, test } from '@jest/globals';
+import { getUUID } from '../../src/plugins/get-id.plugin';
+
+describe('plugins/get-id.plugin', () => {
+  test('getUUID should return a UUID', () => {
+    const uuid = getUUID();
+
+    expect(typeof uuid).toBe('string');
+    expect(uuid.length).toBe(36);
+  });
+});
